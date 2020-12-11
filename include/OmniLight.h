f757ce3923e7b87eec298fb7981c2b78549a3dae
Elijah Kleeman <contact@elijahk.com>
2020-12-10 18:09:39 -0800

    Add `innerRadius` to `OmniLight` to emit photons within a random sphere rather than a single point
    - Soft shadows, ahoy

--- include/OmniLight.h
+++ include/OmniLight.h
@@ -7,5 +7,11 @@ class OmniLight : public Light
 public:
     OmniLight();
 
+    void innerRadius(float innerRadius);
+    float innerRadius() const;
+
     void emit(WorkQueue<Photon>::Block photonBlock) const override;
+
+private:
+    float m_innerRadius = 0;
 };
--- src/OmniLight.cpp
+++ src/OmniLight.cpp
@@ -6,13 +6,28 @@ OmniLight::OmniLight()
     registerType<OmniLight>();
 }
 
+void OmniLight::innerRadius(float innerRadius)
+{
+    m_innerRadius = innerRadius;
+}
+
+float OmniLight::innerRadius() const
+{
+    return m_innerRadius;
+}
+
 void OmniLight::emit(WorkQueue<Photon>::Block photonBlock) const
 {
     for (auto& photon : photonBlock)
     {
         Vector direction = Vector::randomSphere();
 
-        photon.ray = {position(), direction};
+        Vector offset{};
+        if (m_innerRadius > 0)
+        {
+            offset = Vector::random(m_innerRadius);
+        }
+        photon.ray = {position() + offset, direction};
         photon.color = m_color * (m_brightness / photonBlock.size());
     }
 }
