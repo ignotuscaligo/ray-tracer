79ab96b0839c2d060e1aedf93a85a4f831afea7e
Elijah Kleeman <contact@elijahk.com>
2020-12-14 22:35:55 -0800

    fix

--- include/Utility.h
+++ include/Utility.h
@@ -3,8 +3,13 @@
 #include "Vector.h"
 #include "Triangle.h"
 
+namespace Utility
+{
+
 void printVector(const Vector& vector);
 void printTriangle(const Triangle& triangle);
 
 float radians(float degrees);
 float degrees(float radians);
+
+}
